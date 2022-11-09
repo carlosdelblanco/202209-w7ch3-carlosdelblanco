7a90b278265bc8109354df8cdacaf7ac2830b4f7
Jordi Font <jordifv90@gmail.com>
2022-11-09 21:11:44 +0100

    Add auth middleware

---
+++ src/server/middlewares/auth.ts
@@ -0,0 +1,48 @@
+import type { NextFunction, Response } from "express";
+import jwt from "jsonwebtoken";
+import CustomError from "../../CustomError/CustomError.js";
+import type { CustomRequest, UserTokenPayload } from "../types.js";
+
+const auth = (req: CustomRequest, res: Response, next: NextFunction) => {
+  try {
+    const authorizationHeader = req.header("Authorization");
+
+    if (!authorizationHeader) {
+      const error = new CustomError(
+        "Authorization header missing",
+        401,
+        "Missing token"
+      );
+
+      next(error);
+      return;
+    }
+
+    if (!authorizationHeader.startsWith("Bearer ")) {
+      const error = new CustomError(
+        "Missing bearer in Authorization header",
+        401,
+        "Missing token"
+      );
+
+      next(error);
+    }
+
+    const token = authorizationHeader.replace(/^Bearer\s*/, "");
+
+    const user = jwt.verify(token, process.env.SECRET_KEY) as UserTokenPayload;
+
+    req.userId = user.id;
+
+    next();
+  } catch (error: unknown) {
+    const tokenError = new CustomError(
+      (error as Error).message,
+      401,
+      "Invalid token"
+    );
+    next(tokenError);
+  }
+};
+
+export default auth;
