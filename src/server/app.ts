70d08324011df3eb869c50b41c0f7d2bfe443c0f
Jordi Font <jordifv90@gmail.com>
2022-11-09 20:06:22 +0100

    Add users/login route and types

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,12 +2,15 @@ import "../loadEnvironment.js";
 import "../database/databaseConnection.js";
 import express from "express";
 import morgan from "morgan";
+import usersRouter from "./Routes/usersRouter.js";
 
 const app = express();
 
 app.use(morgan("dev"));
 
 app.use(express.json());
+
+app.use("/users", usersRouter);
 
 app.use((req, res) => {
   res.status(200).json({ message: "Hola mundo" });
