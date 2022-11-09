96208e47b952ed28f0afa4179768a44334fae2d3
Jordi Font <jordifv90@gmail.com>
2022-11-09 20:32:25 +0100

    Add registerUser to usersController, type and route

--- src/server/Controllers/usersController.ts
+++ src/server/Controllers/usersController.ts
@@ -1,7 +1,7 @@
 import type { NextFunction, Request, Response } from "express";
 import CustomError from "../../CustomError/CustomError.js";
 import User from "../../database/models/User.js";
-import type { Credentials, UserTokenPayload } from "../types.js";
+import type { Credentials, RegisterData, UserTokenPayload } from "../types.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
@@ -34,4 +34,31 @@ export const loginUser = async (
   const token = jwt.sign(tokenPayload, process.env.SECRET_KEY);
 
   res.status(200).json({ accesToken: token });
+};
+
+export const registerUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { username, password, email } = req.body as RegisterData;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      username,
+      password: hashedPassword,
+      email,
+    });
+
+    res.status(201).json({ user: { id: newUser._id, username, email } });
+  } catch (error: unknown) {
+    const generalError = new CustomError(
+      (error as Error).message,
+      500,
+      "General Pete"
+    );
+    next(generalError);
+  }
 };
