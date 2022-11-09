b3771736b225b8328496fcae48031873e0955a22
Jordi Font <jordifv90@gmail.com>
2022-11-09 20:31:10 +0100

    Add general errors middleware

--- src/server/app.ts
+++ src/server/app.ts
@@ -3,6 +3,7 @@ import "../database/databaseConnection.js";
 import express from "express";
 import morgan from "morgan";
 import usersRouter from "./Routes/usersRouter.js";
+import { generalError, unknownEndpoint } from "./middlewares/errors.js";
 
 const app = express();
 
@@ -15,5 +16,8 @@ app.use("/users", usersRouter);
 app.use((req, res) => {
   res.status(200).json({ message: "Hola mundo" });
 });
+
+app.use(generalError);
+app.use(unknownEndpoint);
 
 export default app;
